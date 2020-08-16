846d160d17235207b0171ec7925bb2d31a5c3f5e
Rishi Kaul <rishikaul@icloud.com>
2020-08-16 19:19:37 +0530

    deploy

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from textblob import TextBlob
 import os
 from flask import Flask, request, jsonify
@@ -11,9 +12,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'assembler'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///' +
-    os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'data.sqlite'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['DEBUG'] = True
 # app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 # app.config['MAIL_PORT'] = 587
@@ -25,7 +27,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 # app.config['MAIL_MAX_EMAILS'] = None
 
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 Migrate(app, db)
 # mail = Mail(app)
 
@@ -44,7 +46,6 @@ Migrate(app, db)
 # app.register_blueprint(hospitals)
 # app.register_blueprint(doctors)
 
-from datetime import datetime
 
 # relate = db.Table('relate',
 #         db.Column('doctor_id', db.Integer, db.ForeignKey('doctors.id')),
@@ -52,55 +53,52 @@ from datetime import datetime
 
 ########### MODELS ##########
 
+
 class Teacher(db.Model):
-    __tablename__='teacher'
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(100), nullable=False)
-    username=db.Column(db.String(50), nullable=False)
-    password=db.Column(db.Text, nullable=False)
-    student_teacher=db.relationship(
+    __tablename__ = 'teacher'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    student_teacher = db.relationship(
         'StudentTeacher', backref='teacherid', lazy=True)
     # services = db.Column(db.ARRAY(String))
 
     def __init__(self, name, username, password):
-        self.name=name
-        self.username=username
-        self.password=password
+        self.name = name
+        self.username = username
+        self.password = password
 
     def __repr__(self):
         return f"Teacher Name is : {self.name}"
 
 
-
 class StudentTeacher(db.Model):
-    __tablename__='studentteacher'
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(20), nullable=False)
-    teacher_name=db.Column(db.String(20), db.ForeignKey(
+    __tablename__ = 'studentteacher'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    teacher_name = db.Column(db.String(20), db.ForeignKey(
         'teacher.username'), nullable=False)
-    presence=db.Column(db.Integer, nullable=False)
-    confidence=db.Column(db.Integer, nullable=False)
-    initiative=db.Column(db.Integer, nullable=False)
-    preperation=db.Column(db.Integer, nullable=False)
-    helpful=db.Column(db.Integer, nullable=False)
-    comments=db.Column(db.Text, nullable=False)
-    rating=db.Column(db.Integer, nullable=False)
-    session_image=db.Column(db.String(64), default='default_image.jpg')
-    date=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-
+    presence = db.Column(db.Integer, nullable=False)
+    confidence = db.Column(db.Integer, nullable=False)
+    initiative = db.Column(db.Integer, nullable=False)
+    preperation = db.Column(db.Integer, nullable=False)
+    helpful = db.Column(db.Integer, nullable=False)
+    comments = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    session_image = db.Column(db.String(64), default='default_image.jpg')
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, name, teacher_name, presence, confidence, initiative, preperation, helpful, comments, rating):
-        self.name=name
-        self.teacher_name=teacher_name
-        self.presence=presence
-        self.confidence=confidence
-        self.initiative=initiative
-        self.preperation=preperation
-        self.helpful=helpful
-        self.comments=comments
-        self.rating=rating
-
+        self.name = name
+        self.teacher_name = teacher_name
+        self.presence = presence
+        self.confidence = confidence
+        self.initiative = initiative
+        self.preperation = preperation
+        self.helpful = helpful
+        self.comments = comments
+        self.rating = rating
 
     # def __repr__(self):
     #     return f"Student  : {self.name}"
@@ -112,6 +110,7 @@ class StudentTeacher(db.Model):
 #         "teacher2" : "password2",
 #         "teacher3" : "password3"}
 
+
 class Login(Resource):
     def post(self):
         try:
@@ -122,13 +121,20 @@ class Login(Resource):
         return {"Status": 'Failure'}
 
 
+class Register(Resource):
+    def post(self):
+        data = request.get_json()
+        teacher = Teacher(data['name'], data['username'], data['password'])
+        db.session.add(teacher)
+        db.session.commit()
+        return {'message': 'Teacher success'}
 
 
 class TeacherApi(Resource):
     def post(self, username):
-        data=request.get_json()
+        data = request.get_json()
 
-        student_teacher=StudentTeacher(
+        student_teacher = StudentTeacher(
             data['name'], data['username'], data['classPresenceValue'],
             data['confidenceValue'], data['initiativeValue'],
             data['preperationValue'], data['helpingValue'],
@@ -139,21 +145,21 @@ class TeacherApi(Resource):
         return {'message': 'Student Teacher created successfully'}
 
     def get(self, username):
-        teacher=Teacher.query.filter_by(username=username).first()
-        student=StudentTeacher.query.filter_by(teacher_name=username).all()
+        teacher = Teacher.query.filter_by(username=username).first()
+        student = StudentTeacher.query.filter_by(teacher_name=username).all()
         # student = StudentTeacher.query.get(1)
         print(student)
-        student_data=[]
+        student_data = []
         for i in range(len(student)):
             student_data.append({'name': student[i].name,
-        'teacher_name': student[i].teacher_name,
-        'presence': student[i].presence,
-        'confidence': student[i].confidence,
-        'initiative': student[i].initiative,
-        'preperation': student[i].preperation,
-        'helpful': student[i].helpful,
-        'comments': student[i].comments,
-        'rating': student[i].rating})
+                                 'teacher_name': student[i].teacher_name,
+                                 'presence': student[i].presence,
+                                 'confidence': student[i].confidence,
+                                 'initiative': student[i].initiative,
+                                 'preperation': student[i].preperation,
+                                 'helpful': student[i].helpful,
+                                 'comments': student[i].comments,
+                                 'rating': student[i].rating})
 
         if not teacher:
             return {'message': 'No teacher found!'}
@@ -161,44 +167,44 @@ class TeacherApi(Resource):
         # student_data['name'] = student.name
         # student_data['confidence'] = student.confidence
 
-        user_data={}
-        user_data['name']=teacher.name
-        user_data['username']=teacher.username
+        user_data = {}
+        user_data['name'] = teacher.name
+        user_data['username'] = teacher.username
 
         return {'teacher': user_data, 'student': student_data}
 
 
-
-
 class Sentiments(Resource):
     def post(self, username):
-        stdata=StudentTeacher.query.filter_by(name=username).all()
-        comments=[]
+        stdata = StudentTeacher.query.filter_by(name=username).all()
+        comments = []
         for i in range(len(stdata)):
             comments.append(stdata[i].comments)
-        polarities=[TextBlob(comment).polarity for comment in comments]
-        avg_polarity=sum(polarities)/len(polarities)
-        review=""
+        polarities = [TextBlob(comment).polarity for comment in comments]
+        avg_polarity = sum(polarities)/len(polarities)
+        review = ""
         if avg_polarity >= -1 and avg_polarity < -0.6:
-            review="Strongly Negative"
+            review = "Strongly Negative"
         elif avg_polarity >= -0.6 and avg_polarity < -0.2:
-            review="Slightly Negative"
+            review = "Slightly Negative"
         elif avg_polarity >= -0.2 and avg_polarity < 0.2:
-            review="Neutral"
+            review = "Neutral"
         elif avg_polarity >= 0.2 and avg_polarity < 0.6:
-            review="Slightly Positive"
+            review = "Slightly Positive"
         else:
-            review="Strongly Positive"
+            review = "Strongly Positive"
 
         return {"review": review, "polarities": avg_polarity}
 
 # db.session.add(Teacher('Rishi','teacher1','abcd'))
 # db.session.commit()
 
-api=Api(app)
+
+api = Api(app)
 api.add_resource(Login, "/login")
 api.add_resource(TeacherApi, '/teacher/<string:username>')
 api.add_resource(Sentiments, '/sentiments/<string:username>')
+api.add_resource(Register, '/addteacher')
 
 
 if __name__ == "__main__":
