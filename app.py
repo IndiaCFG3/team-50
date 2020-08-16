7995bb1026dd4e9395ddd2b5171153cc0c509d6e
Rishi Kaul <rishikaul@icloud.com>
2020-08-16 19:10:07 +0530

    deploy

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'assembler'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///' +
     os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 # app.config['DEBUG'] = True
 # app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 # app.config['MAIL_PORT'] = 587
@@ -25,7 +25,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['MAIL_MAX_EMAILS'] = None
 
 
-db = SQLAlchemy(app)
+db=SQLAlchemy(app)
 Migrate(app, db)
 # mail = Mail(app)
 
@@ -34,7 +34,7 @@ Migrate(app, db)
 # login_manager.init_app(app)
 # login_manager.login_view = 'doctors.login'
 
-#from kindlycare.users.views import users
+# from kindlycare.users.views import users
 # from kindlycare.core.views import core
 # from kindlycare.hospitals.views import hospitals
 # from kindlycare.doctors.views import doctors
@@ -53,18 +53,19 @@ from datetime import datetime
 ########### MODELS ##########
 
 class Teacher(db.Model):
-    __tablename__ = 'teacher'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.Text, nullable=False)
-    student_teacher = db.relationship('StudentTeacher', backref='teacherid', lazy=True)
-    #services = db.Column(db.ARRAY(String))
+    __tablename__='teacher'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100), nullable=False)
+    username=db.Column(db.String(50), nullable=False)
+    password=db.Column(db.Text, nullable=False)
+    student_teacher=db.relationship(
+        'StudentTeacher', backref='teacherid', lazy=True)
+    # services = db.Column(db.ARRAY(String))
 
-    def __init__(self,name,username,password):
-        self.name = name
-        self.username = username
-        self.password = password
+    def __init__(self, name, username, password):
+        self.name=name
+        self.username=username
+        self.password=password
 
     def __repr__(self):
         return f"Teacher Name is : {self.name}"
@@ -72,33 +73,34 @@ class Teacher(db.Model):
 
 
 class StudentTeacher(db.Model):
-    __tablename__ = 'studentteacher'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    teacher_name = db.Column(db.String(20), db.ForeignKey('teacher.username'), nullable=False)
-    presence = db.Column(db.Integer, nullable=False)
-    confidence = db.Column(db.Integer, nullable=False)
-    initiative = db.Column(db.Integer, nullable=False)
-    preperation = db.Column(db.Integer, nullable=False)
-    helpful = db.Column(db.Integer, nullable=False)
-    comments = db.Column(db.Text, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    session_image = db.Column(db.String(64), default='default_image.jpg')
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    __tablename__='studentteacher'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(20), nullable=False)
+    teacher_name=db.Column(db.String(20), db.ForeignKey(
+        'teacher.username'), nullable=False)
+    presence=db.Column(db.Integer, nullable=False)
+    confidence=db.Column(db.Integer, nullable=False)
+    initiative=db.Column(db.Integer, nullable=False)
+    preperation=db.Column(db.Integer, nullable=False)
+    helpful=db.Column(db.Integer, nullable=False)
+    comments=db.Column(db.Text, nullable=False)
+    rating=db.Column(db.Integer, nullable=False)
+    session_image=db.Column(db.String(64), default='default_image.jpg')
+    date=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    
 
-    def __init__(self, name, teacher_name,presence,confidence,initiative,preperation,helpful, comments, rating):
-        self.name = name
-        self.teacher_name = teacher_name
-        self.presence = presence
-        self.confidence = confidence
-        self.initiative = initiative
-        self.preperation = preperation
-        self.helpful = helpful
-        self.comments = comments
-        self.rating = rating
-     
+
+    def __init__(self, name, teacher_name, presence, confidence, initiative, preperation, helpful, comments, rating):
+        self.name=name
+        self.teacher_name=teacher_name
+        self.presence=presence
+        self.confidence=confidence
+        self.initiative=initiative
+        self.preperation=preperation
+        self.helpful=helpful
+        self.comments=comments
+        self.rating=rating
+
 
     # def __repr__(self):
     #     return f"Student  : {self.name}"
@@ -114,87 +116,87 @@ class Login(Resource):
     def post(self):
         try:
             if Teacher.query.filter_by(username=request.json['username']).first().password == request.json['password']:
-                return {"Status" : 'Success'}
+                return {"Status": 'Success'}
         except:
             pass
-        return {"Status" : 'Failure'}
-        
+        return {"Status": 'Failure'}
+
 
 
 
 class TeacherApi(Resource):
     def post(self, username):
-        data = request.get_json()
-        
-        student_teacher = StudentTeacher(
-            data['name'], data['username'], data['classPresenceValue'], 
-            data['confidenceValue'], data['initiativeValue'], 
+        data=request.get_json()
+
+        student_teacher=StudentTeacher(
+            data['name'], data['username'], data['classPresenceValue'],
+            data['confidenceValue'], data['initiativeValue'],
             data['preperationValue'], data['helpingValue'],
             data['feedbackInfo'], data['ratingValue'])
         db.session.add(student_teacher)
         db.session.commit()
 
-        return {'message':'Student Teacher created successfully'}
+        return {'message': 'Student Teacher created successfully'}
 
-    def get(self,username):
-        teacher = Teacher.query.filter_by(username=username).first()
-        student = StudentTeacher.query.filter_by(teacher_name=username).all()
+    def get(self, username):
+        teacher=Teacher.query.filter_by(username=username).first()
+        student=StudentTeacher.query.filter_by(teacher_name=username).all()
         # student = StudentTeacher.query.get(1)
         print(student)
-        student_data = []
+        student_data=[]
         for i in range(len(student)):
-            student_data.append({'name':student[i].name,
-        'teacher_name' : student[i].teacher_name,
-        'presence' : student[i].presence,
-        'confidence' : student[i].confidence,
-        'initiative' : student[i].initiative,
-        'preperation' : student[i].preperation,
-        'helpful' : student[i].helpful,
-        'comments' : student[i].comments,
-        'rating' : student[i].rating})
-        
+            student_data.append({'name': student[i].name,
+        'teacher_name': student[i].teacher_name,
+        'presence': student[i].presence,
+        'confidence': student[i].confidence,
+        'initiative': student[i].initiative,
+        'preperation': student[i].preperation,
+        'helpful': student[i].helpful,
+        'comments': student[i].comments,
+        'rating': student[i].rating})
+
         if not teacher:
             return {'message': 'No teacher found!'}
         # student_data = {}
         # student_data['name'] = student.name
         # student_data['confidence'] = student.confidence
-        
-        user_data = {}
-        user_data['name'] = teacher.name
-        user_data['username'] = teacher.username
 
-        return {'teacher': user_data,'student': student_data}
+        user_data={}
+        user_data['name']=teacher.name
+        user_data['username']=teacher.username
+
+        return {'teacher': user_data, 'student': student_data}
 
 
 
 
 class Sentiments(Resource):
-    def post(self,username):
-        stdata = StudentTeacher.query.filter_by(name = username).all()
-        comments = []
+    def post(self, username):
+        stdata=StudentTeacher.query.filter_by(name=username).all()
+        comments=[]
         for i in range(len(stdata)):
             comments.append(stdata[i].comments)
-        polarities = [TextBlob(comment).polarity for comment in comments]
-        avg_polarity = sum(polarities)/len(polarities)
-        review = ""
+        polarities=[TextBlob(comment).polarity for comment in comments]
+        avg_polarity=sum(polarities)/len(polarities)
+        review=""
         if avg_polarity >= -1 and avg_polarity < -0.6:
-            review = "Strongly Negative"
+            review="Strongly Negative"
         elif avg_polarity >= -0.6 and avg_polarity < -0.2:
-            review = "Slightly Negative"
+            review="Slightly Negative"
         elif avg_polarity >= -0.2 and avg_polarity < 0.2:
-            review = "Neutral"
+            review="Neutral"
         elif avg_polarity >= 0.2 and avg_polarity < 0.6:
-            review = "Slightly Positive"
+            review="Slightly Positive"
         else:
-            review = "Strongly Positive"
+            review="Strongly Positive"
 
-        return {"review" : review, "polarities" : avg_polarity}
+        return {"review": review, "polarities": avg_polarity}
 
-#db.session.add(Teacher('Rishi','teacher1','abcd'))
-#db.session.commit()
+# db.session.add(Teacher('Rishi','teacher1','abcd'))
+# db.session.commit()
 
-api = Api(app)
-api.add_resource(Login,"/login")
+api=Api(app)
+api.add_resource(Login, "/login")
 api.add_resource(TeacherApi, '/teacher/<string:username>')
 api.add_resource(Sentiments, '/sentiments/<string:username>')
 
